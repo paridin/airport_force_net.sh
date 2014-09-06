ec5b79de2071d08f034ba09b9dfd9e144d6d2970
Roberto Estrada <restrada@paridin.com>
2014-09-06 03:56:26 -0500

    First Commit

---
+++ airport_force_net.sh
@@ -0,0 +1,24 @@
+# Configure the network, which you want stay always conected
+NETWORK='paridin.local 5ghz'
+
+#No changed this value unless you want to send the output to shell, this value is for log every time the airport will reboot
+PROD='TRUE'
+air_status=$(/usr/sbin/networksetup -getairportpower en1 | awk '{ print $3 }' | cut -d \( -f2 | cut -d \) -f1)
+
+if [[ $air_status = "en1" ]]; then
+    anet=$(/usr/sbin/networksetup -getairportnetwork en1 | cut -d \: -f2 |  cut -c 2- ) # cleaned the first character always be " "
+
+    if [[ $anet != $NETWORK ]]; then
+        # rebooting
+        if [[ PROD == 'TRUE' ]]; then
+            echo "`date +%Y-%m-%d:%H:%M:%S` - Rebooting airport it was connected at $anet" >> /tmp/reboot_no_correct_net.log
+        else
+            echo "`date +%Y-%m-%d:%H:%M:%S` - Rebooting airport it was connected at $anet"
+        fi
+        /usr/sbin/networksetup -setairportpower en1 off
+        #at this point the airport is down
+
+        /usr/sbin/networksetup -setairportpower en1 on
+        #at this point the airport is up
+    fi
+fi
